246a8516c6734a83a1aec0697cdbdaabbcf8d798
Stephanie Mei <smei826@gmail.com>
2026-05-26 14:00:27 -0400

    js added

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-window.addEventListener("keydown", function (e) {
-  console.log("key pressed:", e.keyCode);
+function playSound(e) {
+  console.log("key Pressed: ", e.keyCode);
 
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log("audio found:", audio);
+  console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add("playing");
 
-  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-});
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
